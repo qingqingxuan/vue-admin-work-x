d56bdd96f9860b1578b3383644d1a84f9d503bbf
596484713@qq.com <liufeng>
2021-05-15 17:31:42 +0800

    初始化仓库

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    />
     <HelloWorld :msg="react.name" />
     <button @click="changeName">改一下名</button>
   </div>
@@ -8,22 +11,22 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-import { ref } from "@vue/reactivity";
+import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from '@vue/reactivity'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld,
+    HelloWorld
   },
   setup() {
     const react = ref({
-      name: "张三",
-    });
+      name: '张三'
+    })
     const changeName = function () {
-      react.name = "new 张三";
-    };
-    return { react, changeName };
-  },
-};
+      react.name = 'new 张三'
+    }
+    return { react, changeName }
+  }
+}
 </script>
