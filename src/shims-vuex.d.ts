b9c2f440810337d647fe1c53946309cf9e4ef61f
596484713@qq.com <liufeng>
2021-06-12 17:43:11 +0800

    对接vuex

--- src/shims-vuex.d.ts
+++ src/shims-vuex.d.ts
@@ -1,8 +1,9 @@
+import { ParentState } from "./store/types";
 import { Store } from "vuex";
 
 declare module "@vue/runtime-core" {
   // 为 `this.$store` 提供类型声明
   interface ComponentCustomProperties {
-    $store: Store;
+    $store: Store<ParentState>;
   }
 }
